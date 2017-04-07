0024e4e33f66895cd327194cca4caf5ac649ccc4
Lieefu Way <lieefu@gmail.com>
2017-04-07 14:10:12 +0800

    lilydict version api,add url

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,6 @@ router.get('/', (req, res) => {
   res.status(200).json({ok:true,message:'api works,ok'});
 });
 router.get('/lilydict/version', (req, res) => {
-  res.status(200).json({ok:true,version:'1.0.1',message:'发现新版本，请下载升级，下载地址:http://lieefu.com/download'});
+  res.status(200).json({ok:true,version:'1.0.1',url:'http://lieefu.com/download',message:'发现新版本，请下载升级'});
 });
 module.exports = router;
