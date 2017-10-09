73dbddf99e1693dccb050da76d2bf046609d6e0f
Lieefu Way <lieefu@gmail.com>
2017-10-09 14:47:43 +0800

    for upgrade ripple wallet to v1.0.1

--- server/api/index.js
+++ server/api/index.js
@@ -10,6 +10,6 @@ router.get('/lilydict/version', (req, res) => {
   res.status(200).json({ok:true,version:'1.0.1',url:'http://lieefu.com/download',message:'发现新版本，请下载升级'});
 });
 router.get('/ripplewallet/version', (req, res) => {
-  res.status(200).json({ok:true,version:'1.0.0',url:'http://lieefu.com/download',message:'发现新版本，请下载升级'});
+  res.status(200).json({ok:true,version:'1.0.1',url:'http://lieefu.com/download',message:'发现新版本，请下载升级,v1.0.1修复转账付款时，增加tag或者备注信息是，支付交易失败bug'});
 });
 module.exports = router;
